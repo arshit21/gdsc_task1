1e6787eef79a80038a27787ea96d53dc5faf7742
Arshit Chaudhary <f20220331@pilani.bits-pilani.ac.in>
2023-09-19 04:24:24 +0530

    Users app

--- quiz/settings.py
+++ quiz/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
+    'quiz_app',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/quiz/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'quiz/static')
 ]
